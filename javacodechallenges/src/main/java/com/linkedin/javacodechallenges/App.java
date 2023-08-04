32810b68edda243201a038b06be30253cae148e0
NicolasCdrt <nicolascoudert4@gmail.com>
2023-08-04 13:43:51 +0000

    implement isPwdComplex

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -4,7 +4,27 @@ import java.util.Scanner;
 
 public class App {
 
+    public static final String numbers = "1234567890";
+
     public static boolean isPasswordComplex(String password) {
+        boolean hasUpperCase = false;
+        boolean hasLowerCase = false;
+        boolean hasNumber = false;
+
+        if (password.length() > 5) {
+            for (int i = 0; i < password.length(); i++) {
+                if (Character.isDigit(password.charAt(i)) && hasNumber != true) {
+                    hasNumber = true;
+                } else if (Character.isLowerCase(password.charAt(i)) && hasLowerCase != true) {
+                    hasLowerCase = true;
+                } else if (Character.isUpperCase(password.charAt(i)) && hasUpperCase != true) {
+                    hasUpperCase = true;
+                }
+            }
+
+            return hasNumber && hasLowerCase && hasUpperCase;
+        }
+
         return false;
     }
 
